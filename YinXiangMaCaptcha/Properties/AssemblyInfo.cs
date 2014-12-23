0539a41babc659aeae869a9a62dd44ada9eb96e5
Kobi Kadosh <yakovka@payoneer.com>
2014-12-23 15:05:06 +0200

    push pack

--- YinXiangMaCaptcha/Properties/AssemblyInfo.cs
+++ YinXiangMaCaptcha/Properties/AssemblyInfo.cs
@@ -32,11 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
-
-#if DEBUG
-[assembly: AssemblyInformationalVersion("1.0.0-alpha")]
-#else
-    [assembly: AssemblyInformationalVersion("1.0.0")]
-#endif
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
